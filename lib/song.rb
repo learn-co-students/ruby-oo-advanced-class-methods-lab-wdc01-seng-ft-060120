eccd2fa55dd6f6436c49832b2fa1a9df1598b6cf
ianmcknnn <ianmcnnn@gmail.com>
2020-06-05 21:46:37 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,56 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+  def self.create
+    @@all << self.new
+    @@all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    (song = self.find_by_name(name)) ? song : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    #there's got to be a better way to do this
+    arr = self.all.each_with_object([]) {|song, a| a << song.name}
+    array = arr.sort!.each_with_object([]) {|name, a| a << self.find_by_name(name)}
+  end
+
+  def self.new_from_filename(filename)
+    artist_name, title = filename.delete_suffix('.mp3').split(' - ')
+    song = self.create_by_name(title)
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def
+    self.all
     @@all
   end
 
