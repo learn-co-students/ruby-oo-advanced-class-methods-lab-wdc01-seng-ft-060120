e178371b6e179d81181e9a4a2ee88129552816e0
andy-aguilar <aaguil3@gmail.com>
2020-06-07 11:51:18 -0400

    wrote methods for song.create and song.new_by_name

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,16 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song 
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
 end
